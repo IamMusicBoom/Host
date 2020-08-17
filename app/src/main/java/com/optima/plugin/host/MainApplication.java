3c1906e2746225c6c152fb4075845f89918560f2
MingAo.Wang <490746560@qq.com>
2020-08-17 10:14:24 +0800

    配置宿主Manifest

---
+++ app/src/main/java/com/optima/plugin/host/MainApplication.java
@@ -0,0 +1,35 @@
+package com.optima.plugin.host;
+
+import android.content.Context;
+
+import com.qihoo360.replugin.RePluginApplication;
+import com.qihoo360.replugin.RePluginConfig;
+import com.wma.baselib.PluginUtils.ContextManager;
+
+/**
+ * create by wma
+ * on 2020/8/13 0013
+ */
+public class MainApplication extends RePluginApplication {
+    String TAG = MainApplication.class.getSimpleName();
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+    }
+
+    @Override
+    protected RePluginConfig createConfig() {
+        RePluginConfig rePluginConfig = new RePluginConfig();
+        rePluginConfig.setUseHostClassIfNotFound(true);
+        return rePluginConfig;
+    }
+}
